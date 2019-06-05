3b42fe179bfaf1f4a7a572d027e2f200b480cbf0
chjysm <chjysm2@naver.com>
2019-06-05 11:42:51 +0900

    asd

--- src/controllers/NaController.java
+++ src/controllers/NaController.java
@@ -73,6 +73,7 @@ public class NaController extends HttpServlet {
 				request.getSession().setAttribute("nickname", null);
 				request.getSession().setAttribute("refresh_token", null);
 				response.sendRedirect("goMain.win");
+				
 			}else if(cmd.equals("/withdrawal.na")) {
 				int id=(int)request.getSession().getAttribute("id");
 				String refresh_token=(String)request.getSession().getAttribute("refresh_token");
